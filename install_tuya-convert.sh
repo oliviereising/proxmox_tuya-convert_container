f3a39dabdc4a7b34dcdddf942603b7a3defc3e4c
Olivier Eising <olivier.eising@hotmail.nl>
2024-11-14 20:44:26 +0100

    chore: Update WLAN interface detection and configuration in install_tuya-convert.sh

--- install_tuya-convert.sh
+++ install_tuya-convert.sh
@@ -65,15 +65,10 @@ git clone --quiet https://github.com/ct-Open-Source/tuya-convert
 
 # Configure tuya-convert
 msg "Configuring tuya-convert..."
-#!/usr/bin/env bash
-
-# Setup script
-set -o errexit  #Exit immediately if a pipeline returns a non-zero status
-set -o errtrace #Trap ERR from shell functions, command substitutions, and commands from subshell
-set -o nounset  #Treat unset variables as an error
-set -o pipefail #Pipe will exit with last non-zero status if applicable
 
 cd /root/tuya-convert
+msg ls
+
 find ./ -name \*.sh -exec sed -i -e "s/sudo \(-\S\+ \)*//" {} \;
 
 WLAN=$(iw dev | sed -n 's/[[:space:]]Interface \(.*\)/\1/p' | head -n 1)
